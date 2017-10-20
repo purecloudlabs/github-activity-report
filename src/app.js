8a59fcfa5632fb1becb3b55dc3c657bb09a6f23b
Tim Smith <tim.j.smith@genesys.com>
2017-10-20 11:39:27 -0600

    add project

---
+++ src/app.js
@@ -0,0 +1,307 @@
+const _ = require('lodash');
+const api = require('github-api-promise');
+const dot = require('dot');
+const fs = require('fs-extra');
+const log = new (require('lognext'))('activity report');
+const moment = require('moment-timezone');
+const path = require('path');
+const Q = require('q');
+
+const github = require('./github-helpers');
+const DATA_CACHE_PATH = path.join(__dirname, '../cache');
+const OUTPUT_PATH = path.join(__dirname, '../output');
+
+
+
+// Set settings
+api.config.token = process.env.GITHUB_TOKEN;
+api.config.debug = process.env.GITHUB_DEBUG_API === 'true';
+dot.templateSettings.strip = false;
+
+var repos = [];
+var repoDataTimestamp = '';
+
+const templateFunctions = {
+	getMoment: (str) => { return moment(str); },
+	diff: (timestamp) => { return moment.duration(moment().diff(timestamp)); }
+};
+
+fs.ensureDirSync(DATA_CACHE_PATH);
+fs.ensureDirSync(OUTPUT_PATH);
+
+
+
+log.profile('data load');
+log.info('Loading data...');
+loadData()
+	.then(() => {
+		log.profile('data load');
+		log.debug(`Processing ${repos.length} repos...`);
+
+		// Sort repos
+		repos = _.sortBy(repos, (repo) => { return repo.lastCommitDate; });
+		repos.reverse();
+
+		// Create data object for template
+		var data = {
+			repos: repos,
+			generatedTimestamp: moment().tz('US/Eastern').format('LLLL z (\\G\\M\\TZ)'),
+			dataTimestamp: repoDataTimestamp.tz('US/Eastern').format('LLLL z (\\G\\M\\TZ)')
+		};
+
+		log.profile('write repo-status-report');
+		let html = executeTemplate(getTemplate('repo-status-report'), data, templateFunctions);
+		fs.writeFileSync(path.join(OUTPUT_PATH, 'repo-status-report.html'), html);
+		log.profile('write repo-status-report');
+	})
+	.catch((err) => {
+		log.error(err);
+	});
+
+
+
+function loadData() {
+	var deferred = Q.defer();
+
+	let cachedDataExists = false;
+	let repoDataPath = path.join(DATA_CACHE_PATH, 'repodata.json');
+	let repoDataTimestampPath = path.join(DATA_CACHE_PATH, 'repodata.timestamp');
+	
+	if (fs.existsSync(repoDataPath) && fs.existsSync(repoDataTimestampPath)) {
+		repoDataTimestamp = moment(fs.readFileSync(repoDataTimestampPath, 'utf8'));
+		let cachedHours = moment().diff(repoDataTimestamp, 'minutes');
+		log.debug(`Cached data is ${cachedHours} minutes old`);
+		cachedDataExists = cachedHours < 12 * 60;
+		if (!cachedDataExists)
+			log.info('Cached data exists, but is out of date.');
+	}
+
+	if (cachedDataExists) {
+		log.info('Loading repo data from cache');
+		repos = require(repoDataPath);
+		deferred.resolve();
+	} else {
+		log.info('Retrieving data from github API. This will take a moment.');
+		loadApiData()
+			.then(() => { deferred.resolve(); })
+			.catch((err) => {
+				log.error(err);
+				deferred.reject(err);
+			});
+	}
+
+	return deferred.promise;
+}
+
+function loadApiData() {
+	let deferred = Q.defer();
+
+	log.profile('repo list');
+	github.getOrgRepos('mypurecloud')
+		.then((data) => {
+			log.profile('repo list');
+			let promises = [];
+
+			// Get pull requests for each repo
+			log.info('Getting repo commits, issues, PRs...');
+			log.profile('repo data');
+			_.forEach(data, (repo) => {
+				repos.push(repo);
+				promises.push(loadRepositoryPullRequests(repo));
+				promises.push(loadRepositoryCommits(repo));
+				promises.push(loadRepositoryIssues(repo));
+			});
+
+			return Promise.all(promises);
+		})
+		.then(() => {
+			log.profile('repo data');
+			let promises = [];
+
+			log.info('Getting PR comments and commits...');
+			log.profile('pr data');
+			for (var i = 0; i < repos.length; i++) {
+				repos[i].pullRequests.forEach((pullRequest) => {
+					promises.push(loadPullRequestCommits(pullRequest));
+					promises.push(loadPullRequestComments(pullRequest));
+				});
+			}
+
+			return Promise.all(promises);
+		})
+		.then(() => {
+			log.profile('pr data');
+			log.debug(`Request Count: ${api.getRequestCount()}`);
+			log.info('Generating repo meta properties...');
+			repos.forEach((repo) => {
+				generateRepositoryMetaProperties(repo);
+			});
+		})
+		.then(() => {
+			repoDataTimestamp = moment();
+			fs.writeFileSync(path.join(DATA_CACHE_PATH, 'repodata.json'), JSON.stringify(repos,null,2));
+			fs.writeFileSync(path.join(DATA_CACHE_PATH, 'repodata.timestamp'), repoDataTimestamp.format());
+			log.info(`Data written to ${DATA_CACHE_PATH}`);
+
+			deferred.resolve();
+		})
+		.catch((err) => {
+			log.error(err);
+			deferred.reject(err);
+		});
+
+	return deferred.promise;
+}
+
+function generateRepositoryMetaProperties(repo) {
+	repo.age = moment.duration(moment().diff(repo.created_at)).humanize();
+
+	// Remove issues that are for a PR
+	let standaloneIssues = [];
+	repo.issues.forEach((issue) => {
+		if (!issue.pull_request)
+			standaloneIssues.push(issue);
+	});
+	repo.issues = standaloneIssues;
+
+	// Find most recent commit
+	let latestCommit = _.maxBy(repo.commits, (commit) => {
+		return commit.commit.author.date;
+	});
+
+	repo.lastCommit = latestCommit;
+	repo.lastCommitDate = latestCommit.commit.author.date;
+	repo.lastCommitDays = moment().diff(latestCommit.commit.author.date, 'days');
+	repo.lastCommitAge = repo.commits.length > 0 ? 
+		moment.duration(moment().diff(latestCommit.commit.author.date)).humanize() + ' ago' :
+		'no commits';
+
+	// Determine repo status
+	if (repo.lastCommitDays < 30) {
+		repo.status = 'active';
+		repo.statusDisplay = 'Active';
+	} else if (repo.lastCommitDays < 180) {
+		repo.status = 'idle';
+		repo.statusDisplay = 'Idle';
+	} else if (repo.lastCommitDays < 365) {
+		repo.status = 'stagnant';
+		repo.statusDisplay = 'Stagnant';
+	} else {
+		repo.status = 'inactive';
+		repo.statusDisplay = 'Inactive';
+	}
+
+	// Set counts
+	repo.commitCount = repo.commits.length; // This isn't useful right now since we only get the first page of commits (limit 30)
+	repo.pullRequestCount = repo.pullRequests.length;
+	repo.issueCount = repo.issues.length;
+
+	// Set extra links
+	repo.pullRequestsUrl = path.join(repo.html_url, 'pulls');
+	if (repo.has_issues)
+		repo.issuesUrl = path.join(repo.html_url, 'issues');
+}
+
+function loadRepositoryIssues(repo) {
+	var deferred = Q.defer();
+
+	api.issues.issues.getRepositoryIssues(repo.owner.login, repo.name)
+		.then((data) => {
+			repo.issues = data;
+			deferred.resolve();
+		})
+		.catch((err) => {
+			log.error(err);
+			deferred.reject(err);
+		});
+
+	return deferred.promise;
+}
+
+function loadRepositoryPullRequests(repo) {
+	var deferred = Q.defer();
+
+	api.pullRequests.pullRequests.getPullRequests(repo.owner.login, repo.name)
+		.then((data) => {
+			repo.pullRequests = data;
+			deferred.resolve();
+		})
+		.catch((err) => {
+			log.error(err);
+			deferred.reject(err);
+		});
+
+	return deferred.promise;
+}
+
+function loadRepositoryCommits(repo) {
+	var deferred = Q.defer();
+
+	api.repositories.commits.getCommits(repo.owner.login, repo.name)
+		.then((data) => {
+			repo.commits = data;
+			deferred.resolve();
+		})
+		.catch((err) => {
+			log.error(err);
+			deferred.reject(err);
+		});
+
+	return deferred.promise;
+}
+
+function loadPullRequestCommits(pullRequest) {
+	var deferred = Q.defer();
+
+	api.pullRequests.pullRequests.getPullRequestCommits(
+		pullRequest.base.user.login, 
+		pullRequest.base.repo.name, 
+		pullRequest.number)
+		.then((data) => {
+			pullRequest.commits = data;
+			deferred.resolve();
+		})
+		.catch((err) => {
+			log.error(err);
+			deferred.reject(err);
+		});
+
+	return deferred.promise;
+}
+
+function loadPullRequestComments(pullRequest) {
+	var deferred = Q.defer();
+
+	api.pullRequests.comments.getPullRequestComments(
+		pullRequest.base.user.login, 
+		pullRequest.base.repo.name, 
+		pullRequest.number)
+		.then((data) => {
+			pullRequest.comments = data;
+			return api.issues.comments.getIssueComments(
+				pullRequest.base.user.login, 
+				pullRequest.base.repo.name, 
+				pullRequest.number);
+		})
+		.then((data) => {
+			pullRequest.comments = pullRequest.comments.concat(data);
+			deferred.resolve();
+		})
+		.catch((err) => {
+			log.error(err);
+			deferred.reject(err);
+		});
+
+	return deferred.promise;
+}
+
+
+
+function executeTemplate(templateString, data, defs) {
+	var template = dot.template(templateString, null, defs);
+	return template(data);
+}
+
+function getTemplate(name) {
+	return fs.readFileSync(path.join(__dirname, `templates/${name}.dot`), 'utf8');
+}
---
+++ src/github-helpers.js
@@ -0,0 +1,77 @@
+const api = require('github-api-promise');
+const log = new (require('lognext'))('github');
+const moment = require('moment-timezone');
+const Q = require('q');
+
+
+
+module.exports = {
+	// Recursively gets events for the given range
+	getEvents: function (username, org, rangeStart, rangeEnd, page = 1, events = [], deferred = Q.defer()) {
+		let self = this;
+		log.debug(`Getting events page ${page}`);
+		api.activity.events.getUserOrganizationEvents(username, org, { page: page })
+			.then(function(res) {
+				events = events.concat(res);
+
+				rangeStart = moment.isMoment(rangeStart) ? rangeStart : moment().subtract(1, 'year');
+				rangeEnd = moment.isMoment(rangeEnd) ? rangeEnd : moment().add(1, 'year');
+
+				let lastEventTimestamp = moment(events[events.length - 1].created_at);
+
+				if (page < 10 && lastEventTimestamp > rangeStart) {
+					self.getEvents(username, org, rangeStart, rangeEnd, page + 1, events, deferred);
+				} else {
+					var finalEvents = [];
+					// Prune results
+					for (let i=0; i<events.length; i++) {
+						let eventTimestamp = moment(events[i].created_at);
+						if (rangeStart <= eventTimestamp && eventTimestamp <= rangeEnd)
+							finalEvents.push(events[i]);
+					}
+
+					log.debug(`${events.length} events pruned to ${finalEvents.length}`);
+
+					deferred.resolve(finalEvents);
+				}
+			})
+			.catch(function(err) {
+				log.error(err);
+				deferred.reject(err);
+			});
+
+		return deferred.promise;
+	},
+
+	// Creates an object with keys=username, values=array of events
+	aggregateEventsByUser: function(events) {
+		let users = {};
+		events.forEach((event) => {
+			if (!users[event.actor.login]) 
+				users[event.actor.login] = [];
+			users[event.actor.login].push(event);
+		});
+		return users;
+	},
+
+	getOrgRepos: function(org, page = 1, repos = [], deferred = Q.defer()) {
+		let self = this;
+		log.debug(`Getting repos page ${page}`);
+		api.repositories.repositories.getOrgRepos(org, { page: page })
+			.then((res) => {
+				if (res.length === 0) {
+					deferred.resolve(repos);
+					return;
+				}
+
+				repos = repos.concat(res);
+				self.getOrgRepos(org, page + 1, repos, deferred);
+			})
+			.catch(function(err) {
+				log.error(err);
+				deferred.reject(err);
+			});
+
+		return deferred.promise;
+	}
+};
